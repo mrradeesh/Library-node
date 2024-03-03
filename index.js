8b496e69eb848000612f1dd0a86e3e476c29c396
mrradeesh <mrradeesh18@gmail.com>
2024-03-03 13:14:38 +0530

    modified

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require("express");
+// const { users } = require("./data/users.json");
+const userRoute = require("./routes/users");
+// const bookRoute = require("./routes/books");
+
 const app = express();
 const PORT = 8080;
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    seccuss: true,
+    success: true,
     message: "Server started",
+  });
+});
+
+app.use("/users", userRoute);
+// app.use("/books", bookRoute);
+app.get("*", (req, res) => {
+  res.status(501).json({
+    Error: "INVALID PATH",
+    message: "not implimented yet",
   });
 });
 
---
+++ routes/users.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const { users } = require("../data/users.json");
+const router = express.Router();
+
+//fetch all the users
+router.get("/", (req, res) => {
+  if (users.length <= 0) {
+    return res.status(200).json({
+      seccuss: true,
+      message: "No one is registered yet",
+    });
+  }
+  res.status(200).json({
+    seccuss: true,
+    message: "User Fetched Seccussfully",
+    // leght: users.users.length(),
+    data: users,
+  });
+});
+//fetch user by id
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  //console.log(id);
+  const user = users.find((each) => each.id == id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "user not found",
+    });
+  }
+  res.status(200).json({
+    sucess: true,
+    message: "user fetched successfully",
+    data: user,
+  });
+});
+//Create New User
+router.post("/", (req, res) => {
+  const { data } = req.body;
+  const user = users.find((each) => each.id == data.id);
+  if (user) {
+    return res.status(409).json({
+      success: false,
+      message: "user id already exist",
+    });
+  }
+  console.log(data);
+  users.push(data);
+  res
+    .status(201)
+    .json({ success: true, message: "user added successfully", data: users });
+});
+//update the user
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+  const user = users.find((each) => each.id === id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not exits to Update",
+    });
+  }
+  const index = users.map((each) => {
+    if (each.id == id) {
+      return { ...each, ...data };
+    }
+    return each;
+  });
+  res.status(200).json({
+    success: true,
+    message: "user updated successfully",
+    data: index,
+  });
+});
+//delete the user
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((each) => each.id == id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "user not found",
+    });
+  }
+  const index = users.indexOf(user);
+  users.splice(index, 1);
+  res.status(200).json({
+    success: true,
+    message: "user deleted successfully",
+    data: users,
+  });
+});
+
+module.exports = router;
